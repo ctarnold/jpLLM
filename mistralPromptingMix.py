ef0b83648a189ca4e3f62f1a106c6ae379cdd178
ctarnold <ctarnold@princeton.edu>
2024-03-17 22:36:48 -0400

    print boolean function result rather than memory location (missing end ())

--- mistralPromptingMix.py
+++ mistralPromptingMix.py
@@ -4,7 +4,7 @@ import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 with open("stderr", "a") as e:
-    print(torch.cuda.is_available, file = e)
+    print(torch.cuda.is_available(), file = e)
 model_id = "/scratch/gpfs/ca2992/Mixtral-8x7B-Instruct-v0.1"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
