4496753bd89100f896015ca7774009487540799c
ctarnold <ctarnold@princeton.edu>
2024-03-17 22:23:14 -0400

    cuda available in gpu. add error check in mix python script

--- mistralPromptingMix.py
+++ mistralPromptingMix.py
@@ -3,6 +3,8 @@ from transformers import BitsAndBytesConfig
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+with open("stderr", "a") as e:
+    print(torch.cuda.is_available, file = e)
 model_id = "/scratch/gpfs/ca2992/Mixtral-8x7B-Instruct-v0.1"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
@@ -21,6 +23,6 @@ messages = [
 
 inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to(device)
 
-outputs = model.generate(inputs, max_new_tokens=512, temperature = 0.7)
+outputs = model.generate(inputs, max_new_tokens=256, temperature = 0.7)
 with open("outputMix.txt", "a") as f:    
     print(tokenizer.decode(outputs[0], skip_special_tokens=True), file = f)
