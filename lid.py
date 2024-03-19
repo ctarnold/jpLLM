f919f0f87d401f8ff0a5c70782b1c2013c09db21
ctarnold <ctarnold@princeton.edu>
2024-03-18 22:34:49 -0400

    fix directory

--- lid.py
+++ lid.py
@@ -4,7 +4,7 @@ text = "me dice ella que trabaha en una tienda de furniture, so anyways that's t
 
 tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-lid-lince")
 
-model = AutoModelForTokenClassification.from_pretrained("sagorsarker/codeswitch-spaeng-lid-lince")
+model = AutoModelForTokenClassification.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-lid-lince")
 lid_model = pipeline('ner', model=model, tokenizer=tokenizer)
 
 lid_model(text)
