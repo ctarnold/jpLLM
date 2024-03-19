0ef4a77317862ceb856efaa3d5898c9d6f65b2bc
ctarnold <ctarnold@princeton.edu>
2024-03-18 22:37:22 -0400

    print lid results to file

--- lid.py
+++ lid.py
@@ -7,4 +7,5 @@ tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaen
 model = AutoModelForTokenClassification.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-lid-lince")
 lid_model = pipeline('ner', model=model, tokenizer=tokenizer)
 
-lid_model(text)
+with open("lidout.txt", "a") as f:
+    print(lid_model(text), file = f)
