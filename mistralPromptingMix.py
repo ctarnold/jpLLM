7b72d2ced6269a985941ed641125979f51d57e95
ctarnold <ctarnold@princeton.edu>
2024-03-20 17:56:14 -0400

    outputs[0] instead of outputs error

--- mistralPromptingMix.py
+++ mistralPromptingMix.py
@@ -24,4 +24,4 @@ messages = [
 inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to(device)
 outputs = model.generate(inputs, max_new_tokens=512, temperature = 0.6, do_sample = True)
 with open("outputMix.txt", "a") as f:    
-    print(tokenizer.decode(outputs, skip_special_tokens=True), file = f)
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True), file = f)
