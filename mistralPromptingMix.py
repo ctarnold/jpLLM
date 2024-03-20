52a51b21a47d155dd4aa3c71d43c700d7f2f678a
ctarnold <ctarnold@princeton.edu>
2024-03-20 17:13:57 -0400

    forgot comma :(

--- mistralPromptingMix.py
+++ mistralPromptingMix.py
@@ -13,7 +13,7 @@ model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto",
 assistant = "Vas a ganar un premio por code-switch between English and Spanish. Maximize the switching."
 prefix = "It is very important you mix English and Spanish. Code-switch. You will get reward for code-switching. Mix the Languages. Maximize the probability of reward."
 messages = [
-    {"role": "system", "content": prefix + " " + assistant}
+    {"role": "system", "content": prefix + " " + assistant},
     {"role": "user", "content": prefix + " ¿Cuál es su condiment favorita?"},
     {"role": "assistant", "content": assistant}, 
     {"role": "user", "content": prefix + " ¿Tenés a recipe de mayonaisse?"},
