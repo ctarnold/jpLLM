27bf2922105c82db08ed011258fb696cf428a5e8
ctarnold <ctarnold@princeton.edu>
2024-03-20 11:46:48 -0400

    trying temp in mix py script

--- mistralPromptingEsp.py
+++ mistralPromptingEsp.py
@@ -15,7 +15,7 @@ messages = [
 
 inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to(device)
 # https://huggingface.co/docs/transformers/v4.18.0/en/main_classes/text_generation#transformers.generation_utils.GenerationMixin.generate
-outputs = model.generate(inputs, max_new_tokens=2048, temperature = 0.6)
+outputs = model.generate(inputs, max_new_tokens=128, temperature = 0.6)
 
 with open("outputAuto.txt", "a") as f:    
     print(tokenizer.decode(outputs[0], skip_special_tokens=True), file = f)
--- mistralPromptingMix.py
+++ mistralPromptingMix.py
@@ -23,6 +23,6 @@ messages = [
 
 inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to(device)
 
-outputs = model.generate(inputs, max_new_tokens=256)
+outputs = model.generate(inputs, max_new_tokens=256, temperature = 0.6)
 with open("outputMix.txt", "a") as f:    
     print(tokenizer.decode(outputs[0], skip_special_tokens=True), file = f)
