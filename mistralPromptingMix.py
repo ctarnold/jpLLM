028dbe5dcb6b6927684e881782c50af2072aad43
ctarnold <ctarnold@princeton.edu>
2024-03-20 17:12:01 -0400

    padding error. remove the term. add system prompt (this may throw error based on experience), keep prefix and assistant reward specifications

--- mistralPromptingMix.py
+++ mistralPromptingMix.py
@@ -13,12 +13,12 @@ model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto",
 assistant = "Vas a ganar un premio por code-switch between English and Spanish. Maximize the switching."
 prefix = "It is very important you mix English and Spanish. Code-switch. You will get reward for code-switching. Mix the Languages. Maximize the probability of reward."
 messages = [
+    {"role": "system", "content": prefix + " " + assistant}
     {"role": "user", "content": prefix + " ¿Cuál es su condiment favorita?"},
     {"role": "assistant", "content": assistant}, 
     {"role": "user", "content": prefix + " ¿Tenés a recipe de mayonaisse?"},
     {"role": "assistant", "content": assistant}, 
-    {"role": "user", "content": prefix  + " Necesito ayuda cocinar algo para mi familia."}, 
-    {"role": "assistant", "content": assistant}
+    {"role": "user", "content": prefix  + " Dame un ejemplo de una manera to cook a good barbecue"}, 
 ]
 
 
@@ -26,7 +26,6 @@ inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to(device)
 
 outputs = model.generate(inputs, max_new_tokens=512, 
                          temperature = 0.6, 
-                         do_sample = True,
-                         padding_side = 'left')
+                         do_sample = True)
 with open("outputMix.txt", "a") as f:    
     print(tokenizer.decode(outputs, skip_special_tokens=True), file = f)
