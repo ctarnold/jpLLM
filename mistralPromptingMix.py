7194cc89e3146b5821978ac27087a57733c48211
ctarnold <ctarnold@princeton.edu>
2024-03-20 20:01:48 -0400

    using 8x7b-v0.1 instead of instruct

--- mistralPromptingMix.py
+++ mistralPromptingMix.py
@@ -5,7 +5,7 @@ import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 with open("stderr", "a") as e:
     print(torch.cuda.is_available(), file = e)
-model_id = "/scratch/gpfs/ca2992/Mixtral-8x7B-Instruct-v0.1"
+model_id = "/scratch/gpfs/ca2992/Mixtral-8x7B-v0.1"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", 
