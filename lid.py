fcaf53492d9b4d30e841d6d40a219a1797831303
ctarnold <ctarnold@princeton.edu>
2024-03-23 15:48:45 -0400

    quick bug fix lid

--- lid.py
+++ lid.py
@@ -9,25 +9,25 @@ lid_model = pipeline('ner', model=model, tokenizer=tokenizer)
 
 out = lid_model(text)
 
-
-spaCount = 0
-engCount = 0
-otherCount = 0
-for i in range(len(out)):
-    language = out[i].get('entity')
-    if (language == 'spa'):
-        spaCount += 1
-    if (language == 'eng'):
-        engCount += 1
-    if (language != 'eng' and language != 'spa'):
-        otherCount += 1
-
 with open("lidout.txt", "a") as f:
-    print(spaCount, file = f)
-    print(" Spanish Count\n", file = f)
-    print(engCount, file = f)
-    print(" English Count\n", file = f)
-    print(otherCount, file = f)
-    print(" Other Count\n", file = f)
+    spaCount = 0
+    engCount = 0
+    otherCount = 0
+    for i in range(len(out)):
+        language = out[i].get('entity')
+        print(language, file = f)
+        if (language == 'spa'):
+            spaCount += 1
+        if (language == 'eng'):
+            engCount += 1
+        if (language != 'eng' and language != 'spa'):
+            otherCount += 1
+
+        print(spaCount, file = f)
+        print(" Spanish Count\n", file = f)
+        print(engCount, file = f)
+        print(" English Count\n", file = f)
+        print(otherCount, file = f)
+        print(" Other Count\n", file = f)
     
     
