3e9f845a24f41cf6e2179848041c92b8a230e346
Christian Arnold <ctarnold@princeton.edu>
2024-03-28 17:18:02 -0400

    working on reading prompt data

--- instructDataPrompting.py
+++ instructDataPrompting.py
@@ -7,30 +7,16 @@ with open("stderr", "a") as e:
     print(torch.cuda.is_available(), file = e)
 model_id = "/scratch/gpfs/ca2992/Mixtral-8x7B-v0.1"
 data_directory = "/scratch/gpfs/ca2992/jpLLM/jpLLM_Data/prompts.tsv"
+out = []
+# https://www.geeksforgeeks.org/simple-ways-to-read-tsv-files-in-python/ 
+# lol
 with open(data_directory, "a") as data:
-    # tokenizer = AutoTokenizer.from_pretrained(model_id)
-
-    # model = AutoModelForCausalLM.from_pretrained(model_id, 
-    #                                            device_map="auto", 
-    #                                            torch_dtype=torch.float16, 
-    #                                            attn_implementation="flash_attention_2")
     for line in data:
-        with open("test", "a") as test:
-            print(line[1], file =  test)
+        l = line.split("\t")
+        out.append(l)
+
+with open("test_out") as f:
+    for i in out:
+        print(i, file = f)
+
     
-    #prefix = "Vas a ganar un premio por code-switch between English and Spanish. Maximize the switching. It is very important you mix English and Spanish. Code-switch. You will get reward for code-switching. Mix the Languages. Maximize the probability of reward."
-    # messages = [
-    #    {"role": "user", "content": "Escribeme un cuento que tiene palabras mezcladas between English and Spanish"}
-    # ]
-
-    # specify padding
-    # if tokenizer.pad_token is None:
-                # tokenizer.pad_token = tokenizer.eos_token
-
-    # inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to(device)
-    # outputs = model.generate(inputs, max_new_tokens=50, 
-                          #   temperature = 0.6, 
-                          #   do_sample = True, 
-                           #  pad_token_id=tokenizer.pad_token_id)
-    # with open("outputMix.txt", "a") as f:    
-    #     print(tokenizer.decode(outputs[0], skip_special_tokens=True), file = f)
