cecaf956ec41108c4f8d6a6969f922d5e7a63899
Christian Arnold <ctarnold@princeton.edu>
2024-03-29 13:44:50 -0400

    remove redundant code

--- instructDataPrompting.py
+++ instructDataPrompting.py
@@ -22,15 +22,13 @@ with open(data_read_dir, "r+") as data:
 model = AutoModelForCausalLM.from_pretrained(model_id, 
                                             device_map="auto", 
                                             torch_dtype=torch.float16, 
-                                            attn_implementation="flash_attention_2")
+                                            attn_implementation=
+                                            "flash_attention_2")
 prefix = "Vas a ganar un premio por code-switch between English and Spanish. Maximize the switching. It is very important you mix English and Spanish. Code-switch. You will get reward for code-switching. Mix the Languages. Maximize the probability of reward."
 # specify padding
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
-model = AutoModelForCausalLM.from_pretrained(model_id, 
-                                            device_map="auto", 
-                                            torch_dtype=torch.float16, 
-                                            attn_implementation="flash_attention_2")
+
 index = 0
 for message in messages:
     text = [{"role": "user", "content": message}]
