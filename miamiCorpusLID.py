e85adec54cae0205a5fc46ebf3269f8b4e5fdb83
Christian Arnold <ctarnold@princeton.edu>
2024-04-11 20:43:01 -0400

    cannot concat type

--- miamiCorpusLID.py
+++ miamiCorpusLID.py
@@ -12,8 +12,7 @@ with open(out, "a") as output:
         with open(data_dir  + '/' + file, "r") as read:
             for line in read:
                 values = line.split()
-                print(values[0] + "---" + values[1] + " --" + 
-                      type(values) + " " + type(values[0]), output)
+                print(values[0] + "---" + values[1], output)
                 # word = values[1]
                 # lidTruth = values[2]
                 # posTruth = values[3]
