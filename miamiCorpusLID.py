239bcd2d76c04abf0086b60175e42090306fa924
Christian Arnold <ctarnold@princeton.edu>
2024-04-11 23:21:49 -0400

    so model output seems capped at length 510. so i will feed input once i hit a period or question mark to the model to do a groundtruth comparison

--- miamiCorpusLID.py
+++ miamiCorpusLID.py
@@ -9,12 +9,28 @@ lid_model = pipeline('ner', model=model, tokenizer=tokenizer)
 data_dir = "/scratch/gpfs/ca2992/jpLLM/bangor/crowdsourced_bangor"
 out = "/scratch/gpfs/ca2992/jpLLM/bangor/test"
 
-
 correctSpa = 0
 correctEn = 0
 wrongSpa = 0
 wrongEn = 0
 other = 0
+
+def groundCompare(text, lidResult):
+     for j in range(len(lidResult)):
+        language = lidResult[j].get('entity')
+        if (language == 'spa'):
+            if (lidGround[j] == 'spa'):
+                correctSpa += 1
+            if (lidGround[j] == 'eng' or lidGround[j] == 'en'):
+                wrongSpa += 1
+        if (language == 'en' or language == 'eng'):
+            if (lidGround[j] == 'eng' or lidGround[j] == 'en'):
+                correctEn += 1
+            if (lidGround[j] == 'spa'):
+                wrongEn += 1
+        if (language != 'en' and language != 'spa' and language != 'eng'):
+                other += 1
+
 with open(out, "a") as output:
     for file in os.listdir(data_dir):
         if os.path.isdir(data_dir  + '/' + file):
@@ -24,6 +40,7 @@ with open(out, "a") as output:
             continue
         with open(data_dir  + '/' + file, "r") as read:
             # get all the speech of the corpus as a gigantic string
+            # lid model i/o is capped at length 510 it seems.
             message = ""
             lidGround = []
             posGround = []
@@ -39,26 +56,14 @@ with open(out, "a") as output:
                 message = message + " " + word + " "
                 lidGround.append(lid)
                 posGround.append(pos)
-            lidResult = lid_model(message)
-            print(len(lidResult))
-            print(len(lidGround))
-            
-            #for j in range(len(lidResult)):
-                # language = lidResult[j].get('entity')
-                # print(language)
-                # if (language == 'spa'):
-                    # if (lidGround[j] == 'spa'):
-                        # correctSpa += 1
-                    # if (lidGround[j] == 'eng' or lidGround[j] == 'en'):
-                        # wrongSpa += 1
-                # if (language == 'en' or language == 'eng'):
-                    # if (lidGround[j] == 'eng' or lidGround[j] == 'en'):
-                        # correctEn += 1
-                    # if (lidGround[j] == 'spa'):
-                        # wrongEn += 1
-                # if (language != 'en' and language != 'spa' and language != 'eng'):
-                        # other += 1
-
+                # with period or question mark get ground truth
+                # comparison
+                if (word == '?' or word == '.'):
+                    lidResult = lid_model(message)
+                    groundCompare(message, lidResult)
+                    message = ""
+                    lidGround = []
+                    posGround = []
 
 
 print(correctSpa)
