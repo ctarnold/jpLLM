87437e7d9366eab859fbfb07ed7e29bd620f1fa3
Christian Arnold <ctarnold@princeton.edu>
2024-04-11 21:07:59 -0400

    print to out file

--- miamiCorpusLID.py
+++ miamiCorpusLID.py
@@ -22,6 +22,6 @@ with open(out, "a") as output:
                 word = values[0]
                 lidTruth = values[1]
                 # posTruth = values[2]
-                print(word + " " + lidTruth + " ", output)
+                print(word + " " + lidTruth + " ", file =  output)
         
     
