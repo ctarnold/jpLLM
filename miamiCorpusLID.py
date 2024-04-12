857d7835aaf3aeef36ea0fa917f2cb5d5ddca39e
Christian Arnold <ctarnold@princeton.edu>
2024-04-11 20:25:08 -0400

    fix concat, add /

--- miamiCorpusLID.py
+++ miamiCorpusLID.py
@@ -5,7 +5,7 @@ out = "/scratch/gpfs/ca2992/jpLLM/bangor/test"
 
 
 for file in os.listdir(data_dir):
-    with open(data_dir + file, "r") as read:
+    with open(data_dir  + '/' + file, "r") as read:
         for line in read:
             with open(out, "a") as f:
                 print(line, f)
