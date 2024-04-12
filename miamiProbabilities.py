6bedc4d806ac7c3200f58826efeebe2e23ad7482
Christian Arnold <ctarnold@princeton.edu>
2024-04-12 13:14:15 -0400

    UPDATE: LAST COMMIT HAD COND. PROB GIVEN POS FOR LAST WORD, DO BASED ON CURRENT WORD POS

--- miamiProbabilities.py
+++ miamiProbabilities.py
@@ -2,7 +2,7 @@ import os
 data_dir = "/scratch/gpfs/ca2992/jpLLM/bangor/crowdsourced_bangor"
 out = "/scratch/gpfs/ca2992/jpLLM/bangor/test"
 
-# switch counts
+# switch counts based on previous word
 EN_SPA_prev_verb = 0 # English to Spanish conditional on prev. word verb
 SPA_EN_prev_verb = 0 # English to Spanish conditional on prev. word verb
 
@@ -11,6 +11,16 @@ SPA_EN_prev_noun = 0 # SPA to EN conditional on prev. word noun
 
 EN_SPA_prev_conj = 0 # EN to SPA conditoinal on prev. word conj
 SPA_EN_prev_conj = 0 # SPA to EN conditional on prev. word conj
+
+# switch counts based on current word
+EN_SPA_verb = 0 # English to Spanish conditional on word verb
+SPA_EN_verb = 0 # English to Spanish conditional on word verb
+
+EN_SPA_noun = 0 # EN to SPA conditional on word noun
+SPA_EN_noun = 0 # SPA to EN conditional on word noun
+
+EN_SPA_conj = 0 # EN to SPA conditoinal on word conj
+SPA_EN_conj = 0 # SPA to EN conditional on word conj
 
 verb_Count = 0
 noun_Count = 0
@@ -65,6 +75,20 @@ with open(out, "a") as output:
                                 EN_SPA_prev_noun += 1
                             if (last_pos == "CONJ"):
                                 EN_SPA_prev_conj += 1
+                        if (last_lid == 'eng'):
+                            if (pos == "VERB"):
+                                EN_SPA_verb += 1
+                            if (pos ==  "NOUN"):
+                                EN_SPA_noun += 1
+                            if (pos == "CONJ"):
+                                EN_SPA_conj += 1
+                        if (last_lid == 'spa'):
+                            if (pos == "VERB"):
+                                SPA_EN_verb += 1
+                            if (pos ==  "NOUN"):
+                                SPA_EN_noun += 1
+                            if (pos == "CONJ"):
+                                SPA_EN_conj += 1
                 if (pos == "VERB"):
                     verb_Count += 1
                 if (pos ==  "NOUN"):
@@ -80,6 +104,12 @@ print((EN_SPA_prev_verb + SPA_EN_prev_verb)/verb_Count)
 print((EN_SPA_prev_noun + SPA_EN_prev_noun)/noun_Count)
 
 print((EN_SPA_prev_conj + SPA_EN_prev_conj)/conj_Count)
+
+print((EN_SPA_verb + SPA_EN_verb)/verb_Count)
+
+print((EN_SPA_noun + SPA_EN_noun)/noun_Count)
+
+print((EN_SPA_conj + SPA_EN_conj)/conj_Count)
 
 
 
