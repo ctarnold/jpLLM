746cfa00f8b1ad21c02a6986ab05477e65ea213d
Christian Arnold <ctarnold@princeton.edu>
2024-04-11 21:25:21 -0400

    gather ground truth data

--- miamiCorpusLID.py
+++ miamiCorpusLID.py
@@ -3,6 +3,10 @@ import os
 data_dir = "/scratch/gpfs/ca2992/jpLLM/bangor/crowdsourced_bangor"
 out = "/scratch/gpfs/ca2992/jpLLM/bangor/test"
 
+# get all the speech of the corpus as a gigantic string?
+message = ""
+lidGround = []
+posGround = []
 
 with open(out, "a") as output:
     for file in os.listdir(data_dir):
@@ -19,9 +23,11 @@ with open(out, "a") as output:
                     continue
                 num = values[0]
                 word = values[1]
-                lidTruth = values[2]
-                posTruth = values[3]
-                print(word + " " + lidTruth + " " + posTruth, 
-                      file = output)
+                lid = values[2]
+                pos = values[3]
+                message = word + " "
+                lidGround.append(lid)
+                posGround.append(pos)
+
         
     
