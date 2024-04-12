ccbb8bdf5c929a49103a7d1419d3af4266a1d132
Christian Arnold <ctarnold@princeton.edu>
2024-04-11 22:55:58 -0400

    compare to ground with each fike

--- miamiCorpusLID.py
+++ miamiCorpusLID.py
@@ -9,11 +9,12 @@ lid_model = pipeline('ner', model=model, tokenizer=tokenizer)
 data_dir = "/scratch/gpfs/ca2992/jpLLM/bangor/crowdsourced_bangor"
 out = "/scratch/gpfs/ca2992/jpLLM/bangor/test"
 
-# get all the speech of the corpus as a gigantic string
-message = ""
-lidGround = []
-posGround = []
 
+correctSpa = 0
+correctEn = 0
+wrongSpa = 0
+wrongEn = 0
+other = 0
 with open(out, "a") as output:
     for file in os.listdir(data_dir):
         if os.path.isdir(data_dir  + '/' + file):
@@ -22,6 +23,10 @@ with open(out, "a") as output:
         if(file == "README.md"):
             continue
         with open(data_dir  + '/' + file, "r") as read:
+            # get all the speech of the corpus as a gigantic string
+            message = ""
+            lidGround = []
+            posGround = []
             for line in read:
                 values = line.split()
                 # skip blank lines
@@ -34,29 +39,22 @@ with open(out, "a") as output:
                 message = message + " " + word + " "
                 lidGround.append(lid)
                 posGround.append(pos)
-
-lidResult = lid_model(message)
-
-correctSpa = 0
-correctEn = 0
-wrongSpa = 0
-wrongEn = 0
-other = 0
-for j in range(len(lidResult)):
-    language = lidResult[j].get('entity')
-    # print(language)
-    if (language == 'spa'):
-        if (lidGround[j] == 'spa'):
-            correctSpa += 1
-        if (lidGround[j] == 'eng' or lidGround[j] == 'en'):
-            wrongSpa += 1
-    if (language == 'en' or language == 'eng'):
-        if (lidGround[j] == 'eng' or lidGround[j] == 'en'):
-            correctEn += 1
-        if (lidGround[j] == 'spa'):
-            wrongEn += 1
-    if (language != 'en' and language != 'spa' and language != 'eng'):
-            other += 1
+            lidResult = lid_model(message)
+            for j in range(len(lidResult)):
+                language = lidResult[j].get('entity')
+                # print(language)
+                if (language == 'spa'):
+                    if (lidGround[j] == 'spa'):
+                        correctSpa += 1
+                    if (lidGround[j] == 'eng' or lidGround[j] == 'en'):
+                        wrongSpa += 1
+                if (language == 'en' or language == 'eng'):
+                    if (lidGround[j] == 'eng' or lidGround[j] == 'en'):
+                        correctEn += 1
+                    if (lidGround[j] == 'spa'):
+                        wrongEn += 1
+                if (language != 'en' and language != 'spa' and language != 'eng'):
+                        other += 1
 
 
 
