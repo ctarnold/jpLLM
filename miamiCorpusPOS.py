de6bf354699f2cd0fcaa997b71632ef64344bf95
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 18:57:12 -0400

    change robertuito directory to that of /models/test_error_model because the new dir has the config.json file

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
 tokenizer = AutoTokenizer.from_pretrained('/scratch/gpfs/ca2992/robertuito/models/beto-cased-finetuned')
-model = AutoModelForTokenClassification.from_pretrained('/scratch/gpfs/ca2992/robertuito/models/beto-cased-finetuned')
+model = AutoModelForTokenClassification.from_pretrained('/scratch/gpfs/ca2992/robertuito/models/test_error_model')
 text = "Esto es un tweet estoy usando pysentimiento"
 
 # ['<s>','▁Esto','▁es','▁un','▁tweet','▁estoy','▁usando','▁','▁hashtag','▁','▁ro','bert','uito','▁@usuario','▁','▁emoji','▁cara','▁revolviéndose','▁de','▁la','▁risa','▁emoji','</s>']
