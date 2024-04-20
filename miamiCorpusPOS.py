34c003aead7b01b3973523591480ac3c48b538e4
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 13:54:56 -0400

    using installed codeswitch lib instead

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,11 +1,5 @@
-import os
-from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
-
-tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
-
-model = AutoModelForTokenClassification.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
-ner_model = pipeline('ner', model=model, tokenizer=tokenizer)
-
-text = "estoy intentando hacer la recognición de entidaded nombeadas. por alguna razón, el resultado no funciona."
-
-print(ner_model(text))
+from codeswitch.codeswitch import NER
+ner = NER('spa-eng')
+text = "estoy intentando hacer que eso funcione correctamente" # your mixed sentence 
+result = ner.tag(text)
+print(result)
