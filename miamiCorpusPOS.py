ca5881a83a4aacc6179c94fa8b1005bd16fd3e79
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 19:25:05 -0400

    sequence to token classification

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,9 +1,9 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
 
 model_name = '/scratch/gpfs/ca2992/robertuito-base-cased'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+model = AutoModelForTokenClassification.from_pretrained(model_name)
 text = "Esto es un tweet estoy usando pysentimiento"
 
 # ['<s>','▁Esto','▁es','▁un','▁tweet','▁estoy','▁usando','▁','▁hashtag','▁','▁ro','bert','uito','▁@usuario','▁','▁emoji','▁cara','▁revolviéndose','▁de','▁la','▁risa','▁emoji','</s>']
