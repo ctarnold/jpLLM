c22108a2c8b46e532b5268266b2fd2f176959900
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 17:13:40 -0400

    compare with lid results, getting blank pos output

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,9 +1,9 @@
 
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
-tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
+tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-lid-lince")
 
-model = AutoModelForTokenClassification.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
+model = AutoModelForTokenClassification.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-lid-lince")
 pos_model = pipeline('ner', model=model, tokenizer=tokenizer)
 
 print(pos_model("put any spanish english code-mixed sentence"))
