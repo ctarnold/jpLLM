40588551be10db2b38f7d7c939ea2673b8a5bfcc
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 13:40:49 -0400

    testing before scaling pos

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -4,8 +4,8 @@ from transformers import AutoTokenizer, AutoModelForTokenClassification, pipelin
 tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
 
 model = AutoModelForTokenClassification.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
-lid_model = pipeline('ner', model=model, tokenizer=tokenizer)
+ner_model = pipeline('ner', model=model, tokenizer=tokenizer)
 
 text = "testing the ner of the model."
 
-print(lid_model(text))
+print(ner_model(text))
