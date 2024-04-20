eae599dd56fbe52b097c1a7f6a48978d2121a33d
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 13:27:50 -0400

    testing pos

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,38 +1,11 @@
 import os
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
-tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-lid-lince")
+tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
 
-model = AutoModelForTokenClassification.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-lid-lince")
-lid_model = pipeline('pos', model=model, tokenizer=tokenizer)
+model = AutoModelForTokenClassification.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
+lid_model = pipeline('ner', model=model, tokenizer=tokenizer)
 
-data_dir = "/scratch/gpfs/ca2992/jpLLM/bangor/crowdsourced_bangor"
-out = "/scratch/gpfs/ca2992/jpLLM/bangor/test"
+text = "testing the ner of the model."
 
-# get all the speech of the corpus as a gigantic string
-message = ""
-lidGround = []
-posGround = []
-
-with open(out, "a") as output:
-    for file in os.listdir(data_dir):
-        if os.path.isdir(data_dir  + '/' + file):
-        # Skip directories
-            continue
-        if(file == "README.md"):
-            continue
-        with open(data_dir  + '/' + file, "r") as read:
-            for line in read:
-                values = line.split()
-                # skip blank lines
-                if (len(values) <= 2):
-                    continue
-                num = values[0]
-                word = values[1]
-                lid = values[2]
-                pos = values[3]
-                message = word + " "
-                lidGround.append(lid)
-                posGround.append(pos)
-
-posResult = lid_model(message)
+print(lid_model(text))
