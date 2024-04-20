3926e95acf51ba3b8baf420431f0a1ec511bfe2f
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 17:03:46 -0400

    testing before scaling pos

---
+++ miamiCorpusPOS.py
@@ -0,0 +1,9 @@
+
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-pos-lince")
+
+model = AutoModelForTokenClassification.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
+pos_model = pipeline('ner', model=model, tokenizer=tokenizer)
+
+pos_model("put any spanish english code-mixed sentence")
