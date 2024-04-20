02ae98bf43142259eedbdc9d14b3f2499b7f154c
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 17:02:37 -0400

    testing before scaling pos

--- miamiCorpusPOS.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
-
-tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
-
-model = AutoModelForTokenClassification.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
-
-ner_model = pipeline('ner', model=model, tokenizer=tokenizer)
-
-print(ner_model("put any spanish english code-mixed sentence, for some reason this is giving blank output when I print it. I'm not sure why this is."))
