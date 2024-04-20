95314e318aacaeb3b140bc32a2a444a333abf8a1
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 19:18:15 -0400

    trying with model direct from hugging face

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
 
-model_name = '/scratch/gpfs/ca2992/robertuito/models/twerto-base-cased'
+model_name = '/scratch/gpfs/ca2992/robertuito-base-cased'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 text = "Esto es un tweet estoy usando pysentimiento"
