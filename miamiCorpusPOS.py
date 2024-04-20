6ca5a5b59d26d2194e24abdddbbfbc9084ab30e1
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 14:09:24 -0400

    try import NER

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,4 +1,4 @@
-from codeswitch.codeswitch import NER
+import NER
 ner = NER('spa-eng')
 text = "estoy intentando hacer que eso funcione correctamente" # your mixed sentence 
 result = ner.tag(text)
