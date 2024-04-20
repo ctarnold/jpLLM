2cb030de5c67c29f471f6d56cf2d8ba5a54a8390
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 17:32:56 -0400

    try pysentimiento

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,9 +1,15 @@
 
-from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+from pysentimiento import create_analyzer
 
-tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
+pos_analyzer = create_analyzer("pos", lang="es")
 
-model = AutoModelForTokenClassification.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
-pos_model = pipeline('ner', model=model, tokenizer=tokenizer)
-
-print(pos_model("put any spanish english code-mixed sentence, london, paris, berlin"))
+pos_analyzer.predict("Quiero que esto funcione correctamente! @perezjotaeme")
+ 
+ 
+>[{'type': 'PROPN', 'text': 'Quiero', 'start': 0, 'end': 6},
+> {'type': 'SCONJ', 'text': 'que', 'start': 7, 'end': 10},
+> {'type': 'PRON', 'text': 'esto', 'start': 11, 'end': 15},
+> {'type': 'VERB', 'text': 'funcione', 'start': 16, 'end': 24},
+> {'type': 'ADV', 'text': 'correctamente', 'start': 25, 'end': 38},
+> {'type': 'PUNCT', 'text': '!', 'start': 38, 'end': 39},
+> {'type': 'NOUN', 'text': '@perezjotaeme', 'start': 40, 'end': 53}]
