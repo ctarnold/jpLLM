3b1ad08442f8c01036c5a876e8473b23ce6dba97
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 14:17:25 -0400

    compute clusters have no internet access, so download model locally and attempt to run

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,5 +1,9 @@
-import NER
-ner = NER('spa-eng')
-text = "estoy intentando hacer que eso funcione correctamente" # your mixed sentence 
-result = ner.tag(text)
-print(result)
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
+
+model = AutoModelForTokenClassification.from_pretrained("/scratch/gpfs/ca2992/codeswitch-spaeng-ner-lince")
+
+ner_model = pipeline('ner', model=model, tokenizer=tokenizer)
+
+print(ner_model("put any spanish english code-mixed sentence"))
