6570395b9134591c6751b9e07b9e3293fc8526e7
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 18:38:29 -0400

    get pysentimiento locally and try running

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,7 +1,11 @@
+from transformers import AutoTokenizer, pipeline
 
-from pysentimiento import create_analyzer
+tokenizer = AutoTokenizer.from_pretrained('/scratch/gpfs/ca2992/robertuito/models/beto-cased-finetuned')
 
-pos_analyzer = create_analyzer("pos", lang="es")
+text = "Esto es un tweet estoy usando #Robertuito @pysentimiento 🤣"
 
-print(pos_analyzer.predict("Quiero que esto funcione correctamente! @perezjotaeme"))
- 
+# ['<s>','▁Esto','▁es','▁un','▁tweet','▁estoy','▁usando','▁','▁hashtag','▁','▁ro','bert','uito','▁@usuario','▁','▁emoji','▁cara','▁revolviéndose','▁de','▁la','▁risa','▁emoji','</s>']
+
+pos_model = pipeline('ner', model=model, tokenizer=tokenizer)
+
+print(pos_model(text))
