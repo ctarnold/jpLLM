969eca99d652d1842e800ea49d7c0fda158b8ca9
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 19:29:05 -0400

    PROGRESS: LAST COMMIT HAD LABELED OUTPUT FOR A GIVEN STRING, BUT HAD LABELS 1 AND 0, TRY USING LIB INSTEAD

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,13 +1,7 @@
-from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+from pysentimiento import create_analyzer
 
+pos_analyzer = create_analyzer("pos", lang="es")
 
-model_name = '/scratch/gpfs/ca2992/robertuito-base-cased'
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForTokenClassification.from_pretrained(model_name)
-text = "Esto es un tweet estoy usando pysentimiento"
-
-# ['<s>','▁Esto','▁es','▁un','▁tweet','▁estoy','▁usando','▁','▁hashtag','▁','▁ro','bert','uito','▁@usuario','▁','▁emoji','▁cara','▁revolviéndose','▁de','▁la','▁risa','▁emoji','</s>']
-
-pos_model = pipeline('ner', model=model, tokenizer=tokenizer)
-
-print(pos_model(text))
+print(pos_analyzer.predict("Quiero que esto funcione correctamente!"))
+ 
+ 
