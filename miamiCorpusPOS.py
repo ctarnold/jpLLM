c7fa6967039038e37fa4a1d141d26a08ea2cc596
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 19:49:49 -0400

    use sagorbrur pos model from hugging face

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,13 +1,14 @@
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
 
-model_name = '/scratch/gpfs/ca2992/robertuito-base-cased'
+# model_name = '/scratch/gpfs/ca2992/robertuito-base-cased'
+model_name = '/scratch/gpfs/ca2992/codeswitch-spaeng-pos-lince'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForTokenClassification.from_pretrained(model_name)
-text = "Esto es un tweet estoy usando pysentimiento"
+text = "Esto es un tweet, estoy codeswitching"
 
 # ['<s>','▁Esto','▁es','▁un','▁tweet','▁estoy','▁usando','▁','▁hashtag','▁','▁ro','bert','uito','▁@usuario','▁','▁emoji','▁cara','▁revolviéndose','▁de','▁la','▁risa','▁emoji','</s>']
 
-pos_model = pipeline('token-classification', model=model, tokenizer=tokenizer)
+pos_model = pipeline('ner', model=model, tokenizer=tokenizer)
 
 print(pos_model(text))
