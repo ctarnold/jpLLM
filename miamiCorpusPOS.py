fa4be64889462d97c1e51227600c780b8096d8fd
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 19:34:50 -0400

    restore old commit at progress checkpoint, but changed task to pos instead of ner

--- miamiCorpusPOS.py
+++ miamiCorpusPOS.py
@@ -1,7 +1,13 @@
-from pysentimiento import create_analyzer
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
-pos_analyzer = create_analyzer("pos", lang="es")
 
-print(pos_analyzer.predict("Quiero que esto funcione correctamente!"))
- 
- 
+model_name = '/scratch/gpfs/ca2992/robertuito-base-cased'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForTokenClassification.from_pretrained(model_name)
+text = "Esto es un tweet estoy usando pysentimiento"
+
+# ['<s>','▁Esto','▁es','▁un','▁tweet','▁estoy','▁usando','▁','▁hashtag','▁','▁ro','bert','uito','▁@usuario','▁','▁emoji','▁cara','▁revolviéndose','▁de','▁la','▁risa','▁emoji','</s>']
+
+pos_model = pipeline('pos', model=model, tokenizer=tokenizer)
+
+print(pos_model(text))
