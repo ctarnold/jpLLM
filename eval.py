ef87bb3c9844b52241793c7050d144850aa15afc
Christian Arnold <ctarnold@princeton.edu>
2024-04-21 11:24:46 -0400

    add eval.py from NLP with Transformers book

---
+++ eval.py
@@ -0,0 +1,14 @@
+# Adapted from O'Reilly's Natural Language Processing with Transformers
+# Building Language Applications with HuggingFace
+# Lewis Tunstall, Leandro von Werra, Thomas Wolf
+# ISBN: 978-1-098-13679-6
+# Page 105
+from seqeval.metrics import classification_report
+y_true = [[]]
+y_pred = [[]]
+print(classification_report(y_true, y_pred))
+
+
+
+
+
