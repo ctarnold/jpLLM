0434355313295dd372ffa6ba5075981a4880e7ea
Christian Arnold <ctarnold@princeton.edu>
2024-04-20 20:04:55 -0400

    POS WORKING, ADD CALLABLE PY FILE

---
+++ posCall.py
@@ -0,0 +1,15 @@
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+
+
+# model_name = '/scratch/gpfs/ca2992/robertuito-base-cased'
+model_name = '/scratch/gpfs/ca2992/codeswitch-spaeng-pos-lince'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForTokenClassification.from_pretrained(model_name)
+text = "Esto es un tweet, estoy codeswitching"
+
+# ['<s>','▁Esto','▁es','▁un','▁tweet','▁estoy','▁usando','▁','▁hashtag','▁','▁ro','bert','uito','▁@usuario','▁','▁emoji','▁cara','▁revolviéndose','▁de','▁la','▁risa','▁emoji','</s>']
+
+pos_model = pipeline('ner', model=model, tokenizer=tokenizer)
+
+def pos(input):
+    return pos_model(input)
