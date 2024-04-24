0760d0a32da4aa37e760e2cfeb22b6f39008fa73
Christian Arnold <ctarnold@princeton.edu>
2024-04-24 09:48:25 -0400

    get data from individual files

--- instructDataPrompting.py
+++ instructDataPrompting.py
@@ -39,7 +39,7 @@ for file in files:
     with open(file, "r+") as f:
         temp = T[index]
         index = index + 1
-        if (index != 2):
+        if (index != 3):
             continue
         promptNum = 0
         for prompt in messages:
