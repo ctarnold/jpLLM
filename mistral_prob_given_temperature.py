baf6569a70273566f1d22c6e3d5e14ec2381b71c
Christian Arnold <ctarnold@princeton.edu>
2024-04-26 14:31:32 -0400

    get switch by lid on model

--- mistral_prob_given_temperature.py
+++ mistral_prob_given_temperature.py
@@ -10,7 +10,9 @@ tokenizer_name = '/scratch/gpfs/ca2992/codeswitch-spaeng-lid-lince'
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 model = AutoModelForTokenClassification.from_pretrained(model_name)
 out_dir = 'mistral_lid_ratios'
+pos_model_name = '/scratch/gpfs/ca2992/codeswitch-spaeng-pos-lince'
 lid_model = pipeline('ner', model=model, tokenizer=tokenizer)
+pos_model = pipeline('ner', model=model, tokenizer = tokenizer)
 
 dir = 'scratch/gpfs/ca2992/jpLLM/jpLLM_Data/'
 files = ['out_t_0_indiv.tsv',
@@ -33,12 +35,14 @@ lid_pred = []
 # convert token predictions to word predictions
 def tokenToWordPred(message, trueWords):
     lidResult = lid_model(message)
+    posResult = pos_model(message)
     index = 0
     for word in trueWords:
         lidToken = lidResult[index].get('word')
         # get the lid predicted for this token and append
         # to the lid word level predictions
         lid = lidResult[index].get('entity')
+
         lid_pred.append([lid])
         # if token word mismatch imlidsible to handle
         if (word != lidToken and word[0] != lidToken[0]):
@@ -73,27 +77,49 @@ def cleanInstruct(text):
 spanish_count = 0
 english_count = 0
 
-out_dir = 'lang_ratio_2'
-
+out_dir = 'lang_lid_ratio'
+switch_verb = 0
+switch_noun = 0
+switch_conj = 0
+switch_count = 0
+count = 0
 for file in files:
     with open(dir + file, "r+") as f:
         for line in f:
-            model_input = ""
             if (line[0] == '['):
                 model_input = cleanInstruct(line)
                 lid_results = lid_model(model_input)
+                pos_results = pos_model(model_input)
+                model_input = ""
                 for i in len(lid_results):
+                    count += 1
                     lid = lid_results[i].get('entity')
+                    pos = pos_results[i].get('entity')
+                    if (i == 0):
+                        last_lid = lid
+                        last_pos = pos
+                    # detect code-switching switch
+                    if (last_lid != lid):
+                        switch_count += 1
+                        if (pos == "VERB"):
+                            switch_verb += 1
+                        if (pos == "NOUN"):
+                            switch_noun += 1
+                        if (pos == "CONJ"):
+                            switch_conj += 1
                     if (lid == 'spa'):
                         spanish_count +=1 
                     if (lid == 'en'):
                         english_count += 1
+                    last_lid = lid
+                    last_pos = pos
             else:
                 model_input += line
-        with open(dir + out_dir, "a") as f:
-            print(file, file = f)
-            print(spanish_count, "Spanish Count", file = f)
-            print(english_count, "English Count", file = f)
+        with open(dir + out_dir, "a") as o:
+            print(file, file = o)
+            print(spanish_count, "Spanish Count", file = o)
+            print(english_count, "English Count", file = o)
+            print(switch_count, switch_noun, switch_conj, switch_verb, file = o)
                 
 
             
